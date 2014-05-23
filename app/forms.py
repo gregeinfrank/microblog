9d7e86c2539155d8cecc0ed706ea2505990a792b
Greg Einfrank <greg.einfrank@venmo.com>
2014-05-22 23:17:15 -0400

    Put real posts in the templates and use pagination

--- app/forms.py
+++ app/forms.py
@@ -9,23 +9,27 @@ class LoginForm(Form):
 
 
 class EditForm(Form):
-        nickname = TextField('nickname', validators = [Required()])
-        about_me = TextAreaField('about_me', validators = [Length(min=0, max=140)])
+    nickname = TextField('nickname', validators = [Required()])
+    about_me = TextAreaField('about_me', validators = [Length(min=0, max=140)])
 
-        def __init__(self, original_nickname, *args, **kwargs):
-            super(EditForm, self).__init__(*args, **kwargs)
-            self.original_nickname = original_nickname
+    def __init__(self, original_nickname, *args, **kwargs):
+        super(EditForm, self).__init__(*args, **kwargs)
+        self.original_nickname = original_nickname
 
-        def validate(self):
-            if not super(EditForm, self).validate():
-                return False
+    def validate(self):
+        if not super(EditForm, self).validate():
+            return False
 
-            if self.nickname.data == self.original_nickname:
-                return True
-
-            user = User.query.filter_by(nickname=self.nickname.data).first()
-            if user is not None:
-                self.nickname.errors.append('This nickname is already in use. Please choose another one.')
-                return False
-
+        if self.nickname.data == self.original_nickname:
             return True
+
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user is not None:
+            self.nickname.errors.append(
+                    'This nickname is already in use. Please choose another one.')
+            return False
+
+        return True
+
+class PostForm(Form):
+    post = TextField('post', validators=[Required()])
--- config.py
+++ config.py
@@ -22,3 +22,6 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+# pagination
+POSTS_PER_PAGE = 2
